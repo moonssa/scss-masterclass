8a499b54b65516aa3696fd6bb9d1864f6b933432
moonk <mkjoehsb@gmail.com>
2022-04-22 20:08:40 +0900

    horror scenes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,17 @@
 import gulp from "gulp";
 import del from "del";
-import sass from "gulp-sass";
+//import sass from "gulp-sass";
 import minify from "gulp-csso";
 import autoprefixer from "gulp-autoprefixer";
 
-sass.compiler = require("node-sass");
+const sass = require("gulp-sass")(require("node-sass"));
 
 const routes = {
   css: {
     watch: "src/scss/*",
     src: "src/scss/styles.scss",
-    dest: "dist/css"
-  }
+    dest: "dist/css",
+  },
 };
 
 const styles = () =>
@@ -21,16 +21,19 @@ const styles = () =>
     .pipe(
       autoprefixer({
         flexbox: true,
-        grid: "autoplace"
-      })
+        grid: "autoplace",
+      }),
     )
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
-const watch = () => {
-  gulp.watch(routes.css.watch, styles);
-};
+// const watch = () => {
+//   gulp.watch(routes.css.watch, styles);
+// };
 
+const watch = () => {
+  gulp.watch(routes.css.watch, { usePolling: true }, styles);
+};
 const clean = () => del(["dist/styles.css"]);
 
 const prepare = gulp.series([clean]);
